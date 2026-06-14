67fd55f88652da1541a07408354866effe8d933c
Saksham Bindal <saksham.bindal2004@gmail.com>
2026-06-14 14:17:08 +0530

    feat: add payload codec interface

---
+++ payload_codec.go
@@ -0,0 +1,7 @@
+package goqueue
+
+// PayloadCodec serializes and deserializes task payloads.
+type PayloadCodec interface {
+	EncodePayload(TaskPayload) ([]byte, error)
+	DecodePayload([]byte) (TaskPayload, error)
+}
