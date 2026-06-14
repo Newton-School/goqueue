fb0057d34ce3693a20d901aec244cce2a11a3ae5
Saksham Bindal <saksham.bindal2004@gmail.com>
2026-06-14 14:20:39 +0530

    feat: add task result contract

---
+++ task_result.go
@@ -0,0 +1,35 @@
+package goqueue
+
+// TaskResult is returned by task handlers.
+type TaskResult struct {
+	State    TaskState
+	Value    any
+	Error    string
+	Metadata map[string]string
+}
+
+// SucceededResult returns a successful task result.
+func SucceededResult(value any) TaskResult {
+	return TaskResult{
+		State: TaskSucceeded,
+		Value: value,
+	}
+}
+
+// FailedResult returns a failed task result.
+func FailedResult(err error) TaskResult {
+	message := ""
+	if err != nil {
+		message = err.Error()
+	}
+
+	return TaskResult{
+		State: TaskFailed,
+		Error: message,
+	}
+}
+
+// Validate verifies result state.
+func (r TaskResult) Validate() error {
+	return ValidateTaskState(r.State)
+}
