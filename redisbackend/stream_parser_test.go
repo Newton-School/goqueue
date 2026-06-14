f1c5dc03035d65d820f7263d094a4813af3f6c47
Saksham Bindal <saksham.bindal2004@gmail.com>
2026-06-14 19:05:04 +0530

    test: cover claimed ready message parsing

--- redisbackend/stream_parser_test.go
+++ redisbackend/stream_parser_test.go
@@ -30,3 +30,25 @@ func TestParseReadyStreamMessagesDecodesMessageField(t *testing.T) {
 		t.Fatalf("StreamID = %q, want 1-0", ready[0].StreamID)
 	}
 }
+
+func TestParseReadyMessagesDecodesClaimedMessages(t *testing.T) {
+	message := testTaskMessage(t)
+	encoded, err := (messageCodec{}).encode(message)
+	if err != nil {
+		t.Fatalf("encode returned error: %v", err)
+	}
+
+	ready, err := parseReadyMessages([]redis.XMessage{{
+		ID:     "2-0",
+		Values: map[string]any{"message": string(encoded)},
+	}})
+	if err != nil {
+		t.Fatalf("parseReadyMessages returned error: %v", err)
+	}
+	if len(ready) != 1 {
+		t.Fatalf("len(ready) = %d, want 1", len(ready))
+	}
+	if ready[0].StreamID != "2-0" {
+		t.Fatalf("StreamID = %q, want 2-0", ready[0].StreamID)
+	}
+}
