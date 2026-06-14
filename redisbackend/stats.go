9f8f04f1b543526f67c43df68f756900cdf8bdd6
Saksham Bindal <saksham.bindal2004@gmail.com>
2026-06-14 14:47:36 +0530

    feat: read Redis queue stats

---
+++ redisbackend/stats.go
@@ -0,0 +1,34 @@
+package redisbackend
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Newton-School/goqueue/backend"
+)
+
+// QueueStats returns ready and scheduled counts for a queue.
+func (b *Backend) QueueStats(ctx context.Context, request backend.QueueStatsRequest) (backend.QueueStats, error) {
+	if b.client == nil {
+		return backend.QueueStats{}, fmt.Errorf("%w: redis client is nil", ErrInvalidRedisOptions)
+	}
+	if err := request.Validate(); err != nil {
+		return backend.QueueStats{}, err
+	}
+
+	readyCount, err := b.client.XLen(ctx, b.keys.readyStream(request.Queue.String())).Result()
+	if err != nil {
+		return backend.QueueStats{}, err
+	}
+
+	scheduledCount, err := b.client.ZCard(ctx, b.keys.scheduledSet(request.Queue.String())).Result()
+	if err != nil {
+		return backend.QueueStats{}, err
+	}
+
+	return backend.QueueStats{
+		Queue:          request.Queue,
+		ReadyCount:     readyCount,
+		ScheduledCount: scheduledCount,
+	}, nil
+}
