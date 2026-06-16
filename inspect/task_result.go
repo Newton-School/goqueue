efa5226d5f11aaf84435926d00b0f1671c5454ce
Saksham Bindal <saksham.bindal2004@gmail.com>
2026-06-16 16:12:28 +0530

    Add task result lookup in inspector

---
+++ inspect/task_result.go
@@ -0,0 +1,23 @@
+package inspect
+
+import (
+	"context"
+
+	"github.com/Newton-School/goqueue/backend"
+	"github.com/Newton-School/goqueue/task"
+)
+
+// TaskResult fetches the latest persisted task result.
+func (i *Inspector) TaskResult(ctx context.Context, taskID task.TaskID) (backend.TaskResultRecord, error) {
+	if i == nil {
+		return backend.TaskResultRecord{}, ErrNilInspector
+	}
+	if i.backend == nil {
+		return backend.TaskResultRecord{}, ErrInspectorBackend
+	}
+	if err := task.ValidateTaskID(taskID.String()); err != nil {
+		return backend.TaskResultRecord{}, err
+ 	}
+
+	return i.backend.GetTaskResult(ctx, taskID)
+}
